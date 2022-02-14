6eb270e2dac5ad5d56e2eef3241c7039985267cc
ngrave01 <nickolas.gravel@tufts.edu>
2022-02-14 14:03:03 -0500

    feat (scanner, toplevel) added whitespace and modified toplevel to allow more than one input

--- scanner.mll
+++ scanner.mll
@@ -16,10 +16,11 @@ let integer = '-'?['0'-'9']['0'-'9']*
 
 (* Entry Points *)
 rule tokenize = parse
-    (* RegEx { action } *)
-    | '-'               { MINUS }
-    | integer           { INT(int_of_string (Lexing.lexeme lexbuf)) }
-    | "#t"              { BOOL(true) }
-    | "#f"              { BOOL(false) }
-    | eof               { EOF }
+  (* RegEx { action } *)
+  | [' ' '\n' '\t' '\r'] { tokenize lexbuf }
+  | '-'                  { MINUS }
+  | integer              { INT(int_of_string (Lexing.lexeme lexbuf)) }
+  | "#t"                 { BOOL(true) }
+  | "#f"                 { BOOL(false) }
+  | eof                  { EOF }
       
--- toplevel.ml
+++ toplevel.ml
@@ -5,18 +5,20 @@ open Ast
 
 (* Evaluator *)
 let rec eval expr =
-    match expr with
-      Int(x) -> string_of_int x
-    | Unary(op, e1) -> 
-        let v1 = (int_of_string (eval e1)) in 
-        let v1 = (-1) * v1
-        in string_of_int v1
-    | Bool(b) -> if b then "#t" else "#f"
+  match expr with
+  | Int(x) -> string_of_int x
+  | Unary(op, e1) -> 
+      let v1 = (int_of_string (eval e1)) in 
+      let v1 = (-1) * v1
+      in string_of_int v1
+  | Bool(b) -> if b then "#t" else "#f"
 
 
 (* Temporary code to print what parser evaluates *)
 let () = 
-    let lex_buf = Lexing.from_channel stdin in
-    let expr = Parser.expr Scanner.tokenize lex_buf in
-    let result = eval expr in
-    print_endline result 
+  let lex_buf = Lexing.from_channel stdin in
+    while true do
+      let expr = Parser.expr Scanner.tokenize lex_buf in
+      let result = eval expr in
+      print_endline result 
+    done
