4e6ff8035d127fab5e251f6d6ddea5a4eff59603
ngrave01 <nickolas.gravel@tufts.edu>
2022-03-31 13:00:19 -0400

    <style> (infer.ml) just changed tabbing to make more 'ocaml' based on docs

--- infer.ml
+++ infer.ml
@@ -24,60 +24,61 @@ let type_infer defns =
 	(* fresh : returns a unique type variable to use as placeholder *)
 	let fresh =
 		let k = ref 0 in
-  		fun () -> incr k; TVar !k 
-  	in
+			fun () -> incr k; TVar !k in
 
-	(* generate_constraints : takes environment and expression and returns a tuple
-		 of (type_variable, constraint_list) *)
-	let rec generate_constraints env expr = match expr with
-		| Literal v -> 
-			(* literal_check : infers the type of a literal and returns a tuple of
-				                 (gtype, constraint_list) *)
-			let literal_check v = match v with
-				| Char _ -> (TChar, [])
-				| Int  _ -> (TInt, [])
-				| Bool _ -> (TBool, []) 
-				| Root r ->
-				let rec tree_check t = match t with
-					| Leaf -> (TTree, [])
-					| Branch (e, t1, t2) -> 
-						let branch_check e t1 t2 =
-							let e, c1 = generate_constraints env e in
-								let t1, c2 = tree_check t1 in 
-									let t2, c3 = tree_check t2 in 
-										let tau = fresh () in
-											(TTree,[(tau, e); (TTree, t1); (TTree, t2)] @ c1 @ c2 @ c3)
-							in branch_check e t1 t2
-					in tree_check r
-				in literal_check v
-		| If (e1, e2, e3) ->
-			let if_check e1 e2 e3 =
-				let t1, c1 = generate_constraints env e1 in
-					let	t2, c2 = generate_constraints env e2 in
-						let t3, c3 = generate_constraints env e3 in
-							let tau = fresh () in
-								(tau, [(TBool, t1); (tau, t2); (tau, t3)] @ c1 @ c2 @ c3)
-				in if_check e1 e2 e3
-    | Var (_) -> (fresh (), [])
-    | Apply (e1, e2) -> 
-    	let apply_check e1 e2  =
-    		let t1, c1 = generate_constraints env e1 in
-    			let t2, c2 = generate_constraints env e2 in 
-    				let tau = fresh() in (tau, (t1, TFunc(t2, tau)) @ c1 @ c2)
-				in apply_check e1 e2
+		(* generate_constraints : takes environment and expression and returns a tuple
+			 of (type_variable, constraint_list) *)
+		let rec generate_constraints env expr =
+			match expr with
+			| Literal v -> 
+				(* literal_check : infers the type of a literal and returns a tuple of
+					                 (gtype, constraint_list) *)
+				let literal_check v =
+					match v with
+					| Char _ -> (TChar, [])
+					| Int  _ -> (TInt, [])
+					| Bool _ -> (TBool, []) 
+					| Root r ->
+						let rec tree_check t =
+						match t with
+						| Leaf -> (TTree, [])
+						| Branch (e, t1, t2) -> 
+								let branch_check e t1 t2 =
+									let (e, c1) = generate_constraints env e in
+										let (t1, c2) = tree_check t1 in 
+											let (t2, c3) = tree_check t2 in 
+												let tau = fresh () in
+													(TTree,[(tau, e); (TTree, t1); (TTree, t2)] @ c1 @ c2 @ c3) in
+									branch_check e t1 t2 in
+					literal_check v
+			| If (e1, e2, e3) ->
+					let if_check e1 e2 e3 =
+						let t1, c1 = generate_constraints env e1 in
+							let	t2, c2 = generate_constraints env e2 in
+								let t3, c3 = generate_constraints env e3 in
+									let tau = fresh () in
+										(tau, [(TBool, t1); (tau, t2); (tau, t3)] @ c1 @ c2 @ c3) in
+						if_check e1 e2 e3
+	    | Var (_) -> (fresh (), [])
+	    | Apply (e1, e2) -> 
+		    	let apply_check e1 e2  =
+		    		let t1, c1 = generate_constraints env e1 in
+		    			let t2, c2 = generate_constraints env e2 in 
+		    				let tau = fresh() in (tau, (t1, TFunc(t2, tau)) @ c1 @ c2) in
+						apply_check e1 e2
 
-    | Let (_, _)   -> raise (Failure ("missing case for type checking"))
-    | Lambda (formals , body) ->
-			let lambda_check f b = 
-				generate_constraints (List.fold_left (fun acc x -> (fresh (), x)::acc) [] f) b
-				in lambda_check formals body
-		in	
-
-		let check_defn env d = match d with
-			| Val (_, e) -> generate_constraints env e 
-			| Expr (e)   -> generate_constraints env e
-			in List.map check_defn defns
-	
+	    | Let (_, _)   -> raise (Failure ("missing case for type checking"))
+	    | Lambda (formals , body) ->
+					let lambda_check f b = 
+						generate_constraints (List.fold_left (fun acc x -> (fresh (), x)::acc) [] f) b in
+						lambda_check formals body in
+						
+			let check_defn env d =
+				match d with
+				| Val (_, e) -> generate_constraints env e 
+				| Expr (e)   -> generate_constraints env e
+				in List.map check_defn defns
+			
 
 (* Probably will map a check-function over the defns (defn list : defs) *)
 (* check-function will take a defn and return an sdefn *)
