6f9d2551073b349f10dca821f1dbea817e561e99
atrinh1996 <amy.bui@tufts.edu>
2022-04-02 16:58:07 -0400

    added pretty print in cast to test it

--- cast.ml
+++ cast.ml
@@ -9,7 +9,7 @@ module StringMap = Map.Make(String)
 
 (* int StringMap.t - for our rho/variable environment 
    (DOES NOT MAP TO VALUES) *)
-type var_env = int StringMap.t
+type var_env = (int * gtype) StringMap.t
 let emptyEnv = StringMap.empty
 
 type cname = string
@@ -53,7 +53,7 @@ type cprog =
 {
     main        : cdefn list;
     functions   : func_env;
-    rho         : var_env; (* could also simply be a string list *)
+    rho         : var_env; 
 }
 
 
@@ -102,7 +102,7 @@ let freeIn exp n =
                                                 false bs))
     | SLambda (formals, body) -> let (_, names) = List.split formals in 
         free body && not (List.fold_left (fun a x -> a || x = n) false names)
-  in free (Void, exp)
+  in free (IType, exp)
 
 (* Given the formals list and body of a lambda (xs, e), and a 
    variable environment, the function returns an environment with only 
@@ -134,8 +134,8 @@ and tree = function
 (* Converts given SVal to CVal, and returns the CVal *)
 let svalToCval (id, (ty, e)) = 
   (* check if id was already defined in rho *)
-  let occurs = if (isBound id res.rho) then (find id res.rho) else 0 in 
-  let _ = bind id (occurs + 1) res.rho in 
+  let (occurs, _) = if (isBound id res.rho) then (find id res.rho) else (0, IType) in 
+  let _ = bind id (occurs + 1, ty) res.rho in 
   let cval = 
   (match e with 
     | SLambda (fformals, fbody) -> 
@@ -148,9 +148,87 @@ let svalToCval (id, (ty, e)) =
             body = sexprToCexpr fbody;
           } 
         in 
-        let _ = StringMap.add   (id ^ string_of_int (find id res.rho)) 
+        let _ = StringMap.add   (id ^ string_of_int (fst (find id res.rho))) 
                                 f_def res.functions
         in None 
     | _-> Some (CVal (id, sexprToCexpr (ty, e)))
   )
   in cval 
+
+(***********************************************************************)
+
+
+(* Pretty Print *)
+let rec string_of_cexpr (ty, e) = 
+    "[" ^ string_of_typ ty ^ " : " ^ string_of_cx e ^ "]"
+and string_of_cx = function 
+    | CLiteral v -> string_of_cvalue v
+    | CVar n -> n  
+    | CIf (e1, e2, e3) -> 
+        "(if "  ^ string_of_cexpr e1 ^ " " 
+                ^ string_of_cexpr e2 ^ " " 
+                ^ string_of_cexpr e3 ^ ")"
+    | CApply (f, args) -> 
+      "(" ^ f ^ " " 
+          ^ String.concat " " (List.map string_of_cexpr args) ^ ")"
+    | CLet (binds, body) -> 
+        let string_of_binding (id, e) = 
+              "[" ^ id ^ " " ^ (string_of_cexpr e) ^ "]"
+        in "(let ("  ^ String.concat " " (List.map string_of_binding binds) 
+                     ^ ") " ^ string_of_cexpr body ^ ")"
+    | CLambda (formals, body) -> 
+        let (tys, names) = List.split formals in 
+        "(lambda (" ^ (List.fold_left2 
+                        (fun space ty para -> string_of_typ ty ^ space ^ para) 
+                        " " tys names) 
+                    ^ ") " 
+                    ^ string_of_cexpr body ^ ")"
+and string_of_cvalue = function
+    | CChar c -> String.make 1 c 
+    | CInt  i -> string_of_int i
+    | CBool b -> if b then "#t" else "#f"
+    | CRoot t -> string_of_ctree t
+and string_of_ctree = function
+    | CLeaf -> "leaf"
+    | CBranch (v, sib, child) -> 
+        "(tree " ^ string_of_cvalue v ^ " " 
+                 ^ string_of_ctree sib ^ " " 
+                 ^ string_of_ctree child ^ ")"
+
+
+let string_of_cdefn = function 
+    | CVal (id, e) -> "(val " ^ id ^ " " ^ string_of_cexpr e ^ ")"
+    | CExpr (cexp) -> string_of_cexpr cexp
+
+let string_of_main main = 
+    String.concat "\n" (List.map string_of_cdefn main) ^ "\n"
+
+let string_of_functions (funcs : func_env) = 
+  let string_of_fdef _ {
+        rettyp = return;
+        fname = fname; 
+        formals = formals;
+        frees = frees;
+        body = body;
+    } ret_string = 
+      let string_of_formal (ty, para) = string_of_typ ty ^ para in
+      let listfrees id (num, ty) l = (ty, id ^ string_of_int num) :: l in 
+      let args = formals @ List.rev (StringMap.fold listfrees frees []) in
+      let def = string_of_typ return ^ " " ^ fname ^ " (" 
+        ^ String.concat ", " (List.map string_of_formal args)
+        ^ ")\n{\n" ^  string_of_cexpr body  ^ "\n}\n"
+      in ret_string ^ def
+  in StringMap.fold string_of_fdef funcs ""
+
+let string_of_rho rho = 
+  StringMap.fold (fun id (num, ty) s -> 
+                    s ^ string_of_typ ty ^ " " ^ id ^ string_of_int num ^ "\n") 
+                 rho ""
+
+let string_of_cprog { main = main; functions = functions; rho = rho } = 
+    print_endline "Main:";
+    print_endline (string_of_main main);
+    print_endline "Functions:";
+    print_endline (string_of_functions functions);
+    print_endline "Rho:";
+    print_endline (string_of_rho rho);
