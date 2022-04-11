a039de2f0a54643c6009508b10bdc74a05d22304
ngrave01 <nickolas.gravel@tufts.edu>
2022-04-11 18:51:40 -0400

    <feat> (infer.ml) if constraint work

--- infer.ml
+++ infer.ml
@@ -1,4 +1,4 @@
-2(* SAST = sdefn list *)
+(* SAST = sdefn list *)
 (* sdefn = (id, sexpr *)
 (* sexpr = (gtype, sx) *)
 (* sx = AST.expr with sexpr inplace of expr *)
@@ -45,7 +45,6 @@ let fresh =
   let k = ref 0 in
 	fun () -> incr k; TParam !k
 
-(* TODO implement this *)
 let sub (theta : (tyvar * gtype) list) (cns : (gtype * gtype) list) =
 	(* sub1 takes in a single constraint and updates it with any substitutions in theta *)
 	let sub1 cn = 
@@ -53,23 +52,19 @@ let sub (theta : (tyvar * gtype) list) (cns : (gtype * gtype) list) =
 			(fun (acc : (gtype * gtype)) (one_sub : tyvar * gtype) ->
 				match acc with
 				| (TYVAR t1, TYVAR t2) -> 
-		      (if (fst one_sub = t1) then (snd one_sub, snd acc)
+		      if (fst one_sub = t1) then (snd one_sub, snd acc)
 					else if (fst one_sub = t2) then (fst acc, snd one_sub)
-					else acc)
+					else acc
 				| (TYVAR t1, _) -> 
-		      (if (fst one_sub = t1) then (snd one_sub, snd acc)
-					else acc)
+		      if (fst one_sub = t1) then (snd one_sub, snd acc)
+					else acc
 				| (_, TYVAR t2) -> 
-		      (if (fst one_sub = t2) then (fst acc, snd one_sub)
-					else acc)
+		      if (fst one_sub = t2) then (fst acc, snd one_sub)
+					else acc
 			| (_, _) -> acc)
 			cn theta in 
 	List.map sub1 cns
 
-(* onesub theta1  = {x, y}
-acc theta2 = {z, n} -> {y, n} *)
-
-(* TODO implement this. Can it be the same as sub or use it somehow? *)
 (* currently applies the substitutions in theta1 to theta2 but TODO do we have
    to reverse it? *)
 let compose theta1 theta2 =
@@ -95,14 +90,15 @@ let rec solve' c1 =
 	| (TYVAR t1, TYVAR t2) -> [(t1, TYVAR t2)]
 	| (TYVAR t, TYCON c) -> [(t, TYCON c)]
 	| (TYVAR t, CONAPP a) -> 	
-			if (List.fold_left (fun acc x -> (is_free_type_var t x || acc)) false (snd a)) 
-															then raise (Type_error "type error")
-															else [(t, CONAPP a)]
+			if List.fold_left 
+				(fun acc x -> (is_free_type_var t x || acc)) false (snd a)
+			then raise (Type_error "type error")
+			else [(t, CONAPP a)]
 	| (TYCON c, TYVAR t) -> solve' (TYVAR t, TYCON c)
 	| (TYCON c1, TYCON c2) -> 
-					if c1 = c2 
-						then []
-						else raise (Type_error "type error: (tycon,tycon)")
+			if c1 = c2 
+			then []
+			else raise (Type_error "type error: (tycon,tycon)")
 	| (TYCON c, CONAPP a) -> raise (Type_error "type error: (tycon, conapp")
 	| (CONAPP a, TYVAR t) -> solve' (TYVAR t, CONAPP a)
 	| (CONAPP a, TYCON c) -> raise (Type_error "type error: (conapp, tycon")
@@ -126,7 +122,11 @@ let rec generate_constraints gctx e =
 		match e with
 		| Literal e -> value e
 		| Var _ -> TYVAR (fresh ()), []
-		| If (e1, e2, e3) -> raise (Type_error "missing case for If")
+		| If (e1, e2, e3) -> 
+			let t1, c1 = generate_constraints gctx e1 in
+			let t2, c2 = generate_constraints gctx e2 in
+			let t3, c3 = generate_constraints gctx e2 in
+			(t3, [((TYCON TBool), t1); (t3, t2)] @ c1 @ c2 @ c3)
 		| Apply (_, _) -> raise (Type_error "missing case for Apply")
 		| Let (_, _) -> raise (Type_error "missing case for Let")
 		| Lambda (_,_) -> raise (Type_error "missing case for Lambda")
@@ -141,6 +141,17 @@ let rec generate_constraints gctx e =
 		| Branch (e, t1, t2) -> raise (Type_error "missing case for Branch")
 	in constrain gctx e
 
+let sub_theta_into_gamma theta gamma = raise (Type_error "function needs to be written")
 
-let type_infer (ctx : 'a StringMap.t ) (d : defn list) =
-	raise (Type_error "not yet implemented")
+(* type_infer should resturn a list of tasts *)
+(* TAST = [ tdefns ] *)
+(* defn list = [ (ident, expr) | expr ]*)
+let rec type_infer (ctx : 'a StringMap.t ) (d : defn list) =
+	match d with
+	| Val (name, e)::ds -> 
+		let result = generate_constraints ctx e in
+		(fst result, snd (type_infer (sub_theta_into_gamma (snd result) ctx) ds))
+	| Expr (e)::ds ->
+		let result = generate_constraints ctx e in
+		(fst result, snd (type_infer (sub_theta_into_gamma (snd result) ctx) ds))
+ 
--- semant.ml
+++ semant.ml
@@ -3,17 +3,3 @@ open Ast
 
 module StringMap = Map.Make(String)
 
-let semantic env dlist =
-
-	let check_defn d = match d with
-		| Val (_, e) -> raise (Failure ("Val not implemented"))
-		| Expr e     -> 
-			let check_expr env e = match e with
-				| Literal         -> SLiteral(e)
-				| Var             -> e
-				| If (e1, e2, e3) -> check_expr env e1; check_expr env e2; check_expr env e3; 
-				| Apply   -> raise (Failure ("Apply not implemented"))
-				| Let     -> raise (Failure ("Let not implemented"))
-				| Lambda  -> raise (Failure ("Lambda not implemented"))
-
-in semantic StringMap.empty defns 
