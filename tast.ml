81d9d09463dc09f3b361ba8816ea23f279f33c1d
ngrave01 <nickolas.gravel@tufts.edu>
2022-05-02 16:50:15 -0400

    <merge> fixed conflicts

--- tast.ml
+++ tast.ml
@@ -19,18 +19,12 @@ type tyscheme = tyvar list * gtype
 let inttype = TYCON TInt
 let chartype = TYCON TChar
 let booltype = TYCON TBool
-<<<<<<< HEAD
-let functiontype resultType formalsTypes = 
-  CONAPP (TArrow resultType, formalsTypes)
-=======
->>>>>>> infer-bug
 
 let functiontype resultType formalsTypes =
   CONAPP (TArrow resultType, formalsTypes)
 
 (* TAST expression *)
 type texpr = gtype * tx
-<<<<<<< HEAD
 and tx = 
   | TLiteral     of tvalue
   | TypedVar     of ident
@@ -53,8 +47,6 @@ type tdefn =
   | TVal of ident * texpr
   | TExpr of texpr
 
-=======
->>>>>>> infer-bug
 
 and tx =
   | TLiteral of tvalue
@@ -83,32 +75,11 @@ and string_of_tycon = function
   | TInt -> "int"
   | TBool -> "bool"
   | TChar -> "char"
-<<<<<<< HEAD
   | TArrow (retty) -> string_of_ttype retty 
 and string_of_tyvar = function
   | TVariable n -> "'" ^ string_of_int n
 and string_of_conapp (tyc, tys) = 
   string_of_tycon tyc ^ " (" ^ String.concat " " (List.map string_of_ttype tys) ^ ")"
-=======
-  | TArrow retty -> string_of_ttype retty
-
-and string_of_tyvar = function TVariable n -> "'" ^ string_of_int n
-
-and string_of_tyscheme (tvlst, gt) =
-  let tvstr = List.map string_of_tyvar tvlst in
-  "([" ^ String.concat ", " tvstr ^ "]" ^ ":" ^ string_of_ttype gt ^ ")"
-
-and string_of_conapp (tyc, tys) =
-  match tyc with
-  | TArrow retty ->
-      "["
-      ^ String.concat " " (List.map string_of_ttype tys)
-      ^ "] -> " ^ string_of_tycon tyc
-  | _ -> raise (Type_error "conapp print")
-
-and string_of_constraint (t1, t2) =
-  "(" ^ string_of_ttype t1 ^ ", " ^ string_of_ttype t2 ^ ")"
->>>>>>> infer-bug
 
 and string_of_constraints cs =
   "[ " ^ String.concat " | , | " (List.map string_of_constraint cs) ^ " ]"
@@ -131,7 +102,6 @@ and string_of_tyformals (gt, ident) =
   "(" ^ ident ^ " : " ^ string_of_ttype gt ^ ")"
 
 (* String of a typed expression (texpr) == (type, t-expression) *)
-<<<<<<< HEAD
 let rec string_of_texpr (typ, exp) = 
   "[" ^ string_of_ttype typ ^ "] " ^ string_of_tx exp
 and string_of_tx = function 
@@ -158,36 +128,6 @@ and string_of_tx = function
     "(lambda (" ^ String.concat ", "  formalStringlist
     ^ ") " ^ string_of_texpr body ^ ")"
 (* toString for Sast.svalue *)
-=======
-let rec string_of_texpr (typ, exp) =
-  "[" ^ string_of_ttype typ ^ "] " ^ string_of_tx exp
-
-and string_of_tx = function
-  | TLiteral v -> string_of_tvalue v
-  | TypedVar id -> id
-  | TypedIf (te1, te2, te3) ->
-      "(if " ^ string_of_texpr te1 ^ " " ^ string_of_texpr te2 ^ " "
-      ^ string_of_texpr te3 ^ ")"
-  | TypedApply (f, args) ->
-      "(" ^ string_of_texpr f ^ " ["
-      ^ String.concat " -> " (List.map string_of_texpr args)
-      ^ "])"
-  | TypedLet (binds, body) ->
-      let string_of_binding (id, e) =
-        "[" ^ id ^ " " ^ string_of_texpr e ^ "]"
-      in
-      "(let ("
-      ^ String.concat " " (List.map string_of_binding binds)
-      ^ ") " ^ string_of_texpr body ^ ")"
-  | TypedLambda (formals, body) ->
-      let formalStringlist =
-        List.map (fun (ty, x) -> string_of_ttype ty ^ " " ^ x) formals
-      in
-      "(lambda ("
-      ^ String.concat ", " formalStringlist
-      ^ ") " ^ string_of_texpr body ^ ")"
-
->>>>>>> infer-bug
 and string_of_tvalue = function
   | TChar c -> String.make 1 c
   | TInt i -> string_of_int i
@@ -196,7 +136,6 @@ and string_of_tvalue = function
 
 (* toString for Sast.stree *)
 and string_of_ttree = function
-<<<<<<< HEAD
     TLeaf -> "leaf"
   | TBranch (v, sib, child) -> 
     "(tree " ^ string_of_tvalue v ^ " " 
@@ -204,25 +143,12 @@ and string_of_ttree = function
     ^ string_of_ttree child ^ ")"
 
 
-=======
-  | TLeaf -> "leaf"
-  | TBranch (v, sib, child) ->
-      "(tree " ^ string_of_tvalue v ^ " " ^ string_of_ttree sib ^ " "
-      ^ string_of_ttree child ^ ")"
->>>>>>> infer-bug
 
 (* String of a typed defn (tdefn) *)
 let string_of_tdefn = function
   | TVal (id, te) -> "(val " ^ id ^ " " ^ string_of_texpr te ^ ")"
-<<<<<<< HEAD
   | TExpr te    -> string_of_texpr te
 
 (* String of the tprog == tdefn list *)
 let string_of_tprog tdefns = 
-=======
-  | TExpr te -> string_of_texpr te
-
-(* String of the tprog == tdefn list *)
-let string_of_tprog tdefns =
->>>>>>> infer-bug
   String.concat "\n" (List.map string_of_tdefn tdefns) ^ "\n"
