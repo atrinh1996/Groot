926506158d3ca1269520d4412facff7cf016c168
atrinh1996 <amy.bui@tufts.edu>
2022-05-05 17:07:56 -0400

    adding code to final appendix

--- scope.ml
+++ scope.ml
@@ -1,6 +1,5 @@
 (* Name (scope) checks variable names *)
 open Ast
-exception Unbound of string
 
 (* toplevel naming environment, preloaded with built-ins *)
 let nameEnv = List.fold_right List.cons [ "printi"; "printb"; "printc";
